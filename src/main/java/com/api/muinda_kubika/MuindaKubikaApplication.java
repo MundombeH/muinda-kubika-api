67d4ffc0c36cbdc6a61ab24fca05306051860887
Your Name <120112177+MundombeH@users.noreply.github.com>
2026-06-07 13:18:59 +0100

    primeiro commit

---
+++ src/main/java/com/api/muinda_kubika/MuindaKubikaApplication.java
@@ -0,0 +1,13 @@
+package com.api.muinda_kubika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuindaKubikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuindaKubikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/muinda_kubika/MuindaKubikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.muinda_kubika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuindaKubikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
